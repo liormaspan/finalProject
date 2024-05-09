e02d59d235dbd7b1e26e46c3c8b00f1731700501
liormaspan <liormaspan@gmail.com>
2024-05-09 09:21:21 +0300

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const cors=require('cors');
 const router = require('./router/gameRouter');
+const path=require("path")
 
 const app=express()
 app.use(cors())
@@ -16,3 +17,9 @@ app.use('/',router)
 app.use('/numbers',router)
 app.use('/colors',router)
 app.use('/animals',router)
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
