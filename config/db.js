0646a9c850b6d4fa96d77dd8132057308c0acefd
liormaspan <liormaspan@gmail.com>
2024-05-22 10:03:04 +0300

    update

--- config/db.js
+++ config/db.js
@@ -1,16 +1,23 @@
-const knex = require('knex');
-const dotenv = require('dotenv');
-dotenv.config();
+const postgres = require('postgres');
+require('dotenv').config();
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
-    },
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
 });
 
-module.exports = { db };
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use('/numbers',router)
 app.use('/colors',router)
 app.use('/animals',router)
 
-// const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
